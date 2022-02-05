665a7ac79bf439ecd5a1cf51253379179c2abf44
Tanya Gupta <tanyagupta.pg@gmail.com>
2022-02-05 11:35:08 +0530

    Added Number Sequence

---
+++ Sheet Questions/11-Number-Sequence.py
@@ -0,0 +1,18 @@
+
+list = input().split(',')
+l = len(list)
+
+sum1 = 0
+sum2 = ''
+
+idxFive = list.index('5')
+idxEight = list.index('8')
+
+for i in range(0, l):
+    if i < idxFive or i > idxEight:
+        sum1 += int(list[i])
+
+for i in range(idxFive, idxEight + 1):
+    sum2 += ''.join(list[i])
+
+print(sum1 + int(sum2))
